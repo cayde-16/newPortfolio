9f68cae7939ab3d90dea7055019842baa2da48d5
Cade Heck <cadeheck16@gmail.com>
2022-09-27 13:16:24 -0400

    setting allowed host

--- new_portfolio/settings.py
+++ new_portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=1*0tagy84p+s3p4q_1h#a!gh5!ly(ouu$w)a(vj!a*t)1b_^f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0','cadesportfolio.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0','cadeportfolio.herokuapp.com']
 
 
 # Application definition
