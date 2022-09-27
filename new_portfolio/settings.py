9edd522c99080f88baa8ebe1e1be21d2fdb95833
Cade Heck <cadeheck16@gmail.com>
2022-09-27 13:29:23 -0400

    updated DEBUG

--- new_portfolio/settings.py
+++ new_portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=1*0tagy84p+s3p4q_1h#a!gh5!ly(ouu$w)a(vj!a*t)1b_^f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0','cadeportfolio.herokuapp.com']
 
